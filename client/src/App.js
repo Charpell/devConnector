38f6f082e6999dfc575d6e2a8b382204551ac49d
Charpell <charpellumeh@gmail.com>
2018-07-07 07:52:29 +0100

    Create Posts architecture, Component, Actions, Reducer to create post

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
@@ -92,6 +93,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
             </div>
             <Footer />
